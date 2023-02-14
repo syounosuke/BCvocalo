5033e0a5179bf4b43379c0011c2495ad0cab1733
syounosuke <nakatani@nakatanikiko.com>
2023-02-14 16:54:33 +0900

    main

--- src/App.js
+++ src/App.js
@@ -155,8 +155,8 @@ function App() {
 
   const decrementMintAmount = () => {
     let newMintAmount = mintAmount - 1;
-    if (newMintAmount < 10) {
-      newMintAmount = 10;
+    if (newMintAmount < 1) {
+      newMintAmount = 1;
     }
     setMintAmount(newMintAmount);
   };
